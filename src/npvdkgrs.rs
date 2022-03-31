efcc70c140af1d34bf4c26a11077bdf5c45c716e
PopcornPaws <lipschitz77@gmail.com>
2022-03-31 22:24:59 +0200

    WIP share stuff.

--- src/npvdkgrs.rs
+++ src/npvdkgrs.rs
@@ -5,45 +5,48 @@ use bls::G2Affine;
 use rand_core::RngCore;
 
 pub struct Contribution {
-    pub contributor: Participant,
-    pub contributions: Vec<(Participant, PvshProof)>,
-    pub public_shares: Vec<G2Affine>,
+    pub participant: Participant,
+    pub proof: PvshProof,
+    pub public_share: G2Affine,
+}
+pub struct Npvdkgrs<const N: usize, const T: usize>;
+
+impl<const N: usize, const T: usize> Npvdkgrs<N, T> {
+    const SIZE_CHECK: () = assert!(N >= T, "invalid threshold");
+
+    pub fn contributions<R: RngCore>(
+        rng: &mut R,
+        participants: Vec<Participant>,
+        me: Participant,
+        old_share: Option<Share>,
+    ) -> Vec<Contribution> {
+        let _ = Self::SIZE_CHECK;
+        todo!();
+    }
 }
 
-pub fn calculate_contribution<R: RngCore>(
-    rng: &mut R,
-    threshold: usize,
-    me: Participant,
-    participants: Vec<Participant>,
-    old_share: Option<Share>,
-) -> Contribution {
-    assert!(
-        threshold <= participants.len(),
-        "threshold cannot be greater than the nubmer of participants"
-    );
-
-    // "me" should be included in participants
-    let mut shares = Vec::<Share>::with_capacity(participants.len());
-
-    if let Some(sh) = old_share {
-        shares.push(sh);
-    }
-
-    while shares.len() < threshold {
-        shares.push(Share::random(rng));
-    }
-
-    for participant in participants.into_iter() {
-        // TODO lagrange interpolation
-    }
-
-    //let mut contribution = Contribution {
-    //    contributor: me,
-    //    contributions: Vec::<(Participant, PvshProof)>::with_capacity(participants.len()),
-    //};
-    todo!()
-}
-
-pub fn calculate_my_key() -> CalculatedShare {
-    todo!();
-}
+//    // "me" should be included in participants
+//    let mut shares = Vec::<Share>::with_capacity(participants.len());
+//
+//    if let Some(sh) = old_share {
+//        shares.push(sh);
+//    }
+//
+//    while shares.len() < threshold {
+//        shares.push(Share::random(rng));
+//    }
+//
+//    for participant in participants.into_iter() {
+//        // TODO lagrange interpolation
+//    }
+//
+//    //let mut contribution = Contribution {
+//    //    contributor: me,
+//    //    contributions: Vec::<(Participant, PvshProof)>::with_capacity(participants.len()),
+//    //};
+//    todo!()
+//}
+//
+//pub fn calculate_my_key() -> CalculatedShare {
+//    todo!();
+//}
--- src/polynomial.rs
+++ src/polynomial.rs
@@ -5,37 +5,26 @@ use bls::Scalar;
 use ff::Field;
 use rand_core::RngCore;
 
-// Is this really necessary?
-// DEG - polynomial degree
-pub struct Polynomial<const DEG: usize>([Scalar; DEG + 1])
-where
-    [(); DEG + 1]: Sized;
-
-impl<const DEG: usize> Polynomial<DEG>
-where
-    [(); DEG + 1]: Sized,
-{
-    pub fn new<R: RngCore>(rng: &mut R, secret: Scalar) -> Self {
-        // fill coeffs with secret
-        let mut coeffs = [secret; DEG + 1];
-        // override elements with a random coeff
-        // 0^th element remains the secret
-        for i in 1..DEG {
-            coeffs[i] = Scalar::random(&mut *rng);
-        }
-        Self(coeffs)
+pub fn share<const DEG: usize, R: RngCore>(rng: &mut R, secret: Scalar) -> Scalar {
+    // fill coeffs with secret
+    let mut coeffs = [secret; DEG + 1];
+    // override elements with a random coeff
+    // 0^th element remains the secret
+    for i in 1..DEG {
+        coeffs[i] = Scalar::random(&mut *rng);
     }
-
-    pub fn evaluate(&self, at: Scalar) -> Scalar {
-        self.0
-            .iter()
-            .enumerate()
-            .fold(Scalar::zero(), |acc, (i, x)| {
-                let quotient = &[i as u64, 0, 0, 0]; // little endian repr.
-                acc + x.pow(quotient)
-            })
-    }
+    todo!();
 }
+
+//    pub fn evaluate(&self, at: Scalar) -> Scalar {
+//        self.0
+//            .iter()
+//            .enumerate()
+//            .fold(Scalar::zero(), |acc, (i, x)| {
+//                let quotient = &[i as u64, 0, 0, 0]; // little endian repr.
+//                acc + x.pow(quotient)
+//            })
+//    }
 
 pub struct Point {
     x: Scalar,
